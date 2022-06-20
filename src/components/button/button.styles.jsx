28d5a57f5623f4d4f08514784ff0ddb3a2532532
Mohsin Braer <hbraer@gmail.com>
2022-06-20 17:50:41 -0400

    updates to css, use of styled based CSS for buttons

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,6 @@
-.button-container {
+import styled from 'styled-components';
+
+export const BaseButton = styled.button`
     min-width: 165px;
     width: auto;
     height: 50px;
@@ -16,34 +18,34 @@
     display: flex;
     justify-content: center;
     border-radius: 12px;
-  
+
     &:hover {
       background-color: white;
       color: black;
       border: 1px solid black;
     }
-  
-    &.google-sign-in {
-      background-color: #4285f4;
-      color: white;
-  
-      &:hover {
-        background-color: white;
-        color: #4285f4;
-        border: 1px solid #4285f4;
-      }
-    }
-  
-    &.inverted {
+
+`
+
+export const GoogleSignInButton = styled(BaseButton)`
+    background-color: #4285f4;
+    color: white;
+
+    &:hover {
       background-color: white;
-      color: black;
-      border: 1px solid black;
-  
-      &:hover {
-        background-color: black;
-        color: white;
-        border: none;
-      }
+      color: #4285f4;
+      border: 1px solid #4285f4;
     }
-  }
+`
+export const InvertedButton = styled(BaseButton)`
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+
+    &:hover {
+      background-color: black;
+      color: white;
+      border: none;
+    }
+`
   
--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { signInAuthUserWithEmailAndPassword, createUserDocumentFromAuth, signInWithGooglePopup} from '../../utils/firebase/firebase.utils'
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
 import './sign-in-form.styles.scss'
 
@@ -67,7 +67,7 @@ const SignInForm = () => {
           <div className='buttons-container'>
             <Button type='submit'>Sign In</Button>
             
-            <Button buttonType='google' type='button' onClick={signInWithGoogle}>
+            <Button buttonType={BUTTON_TYPE_CLASSES.google} type='button' onClick={signInWithGoogle}>
               Sign In With Google
             </Button>
           </div>
